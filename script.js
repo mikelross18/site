089a1ff46e7bea1ca2385a3ad47bde397648f1ee
mikelross18 <80691814+mikelross18@users.noreply.github.com>
2021-07-22 19:32:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var navLinks = document.getElementById("navLinks");
 function showMenu()
 {
     navLinks.style.right = "0";
-    
+    vissible: 0;
 }
 function hideMenu()
 {
     navLinks.style.right = "-200px";
-    
+    vissible: 1;
 }
