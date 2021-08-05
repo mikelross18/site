4b401ee3c52089a5eccbc40eef834785ecbc130c
mikelross18 <80691814+mikelross18@users.noreply.github.com>
2021-08-05 19:44:04 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ var navLinks = document.getElementById("navLinks");
 
 function showMenu()
 {
-    navLinks.style.right = "100px";
-    display: none;
+    navLinks.style.right = "0";
 }
 function hideMenu()
 {
     navLinks.style.right = "-200px";
-    
 }
